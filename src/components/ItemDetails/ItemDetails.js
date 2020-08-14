f36b19b806018e3882c0a1e7f8040b5b8674f924
AliKamal <alikamal2381@gmail.com>
2020-08-14 17:49:06 +0500

    final-beta-version

---
+++ src/components/ItemDetails/ItemDetails.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { Button, Collapse, Media, Row, Col } from "react-bootstrap";
+
+class ItemDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Button
+          className="item-details-button"
+          variant="link"
+          onClick={() => this.setState({ open: !this.state.open })}
+        >
+          {this.state.open === false ? `See` : `Hide `} item details
+          {this.state.open === false ? ` +` : ` -`}
+        </Button>
+        <Collapse in={this.state.open}>
+          <div>
+            <Media>
+              <img
+                width={100}
+                height={100}
+                alt="thumbnail"
+                src="https://i5.walmartimages.com/asr/11ff67a3-5836-4ba7-94b5-51318bd3430b_1.89edb4386521d7009a9ba1de1b7ab034.jpeg?odnWidth=undefined&odnHeight=undefined&odnBg=ffffff"
+              />
+              <Media.Body>
+                <p>
+                  Essentials by OFM ESS-3085 Racing Style Leather Gaming Chair,
+                  Red
+                </p>
+                <Row className="show-grid">
+                  <Col md={6}>
+                    <strong>{`$${this.props.price}`}</strong>
+                    <br />
+                    <strong className="price-strike">
+                      {`$${this.props.price}`}
+                    </strong>
+                  </Col>
+                  <Col md={6}>Qty: 1</Col>
+                </Row>
+              </Media.Body>
+            </Media>
+          </div>
+        </Collapse>
+      </div>
+    );
+  }
+}
+
+export default ItemDetails;
